8b6dc386c7fbe5847db71572403e5f6fde9f5d88
Shineaki <halmosadam92@gmail.com>
2023-02-06 23:14:20 +0100

    feat: :art: Init

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
+    name = os.environ.get("NAME", "Adam")
     return f"Hello {name}!"
-
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
